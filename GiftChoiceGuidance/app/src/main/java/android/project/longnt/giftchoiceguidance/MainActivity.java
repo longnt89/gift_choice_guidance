21a62eba53b1e23902e2c6d4dc9ebe5114c8cb07
thanh-long.nguyen <thanh-long.nguyen@horanet.com>
2017-12-19 12:32:21 +0100

    Init add

---
+++ GiftChoiceGuidance/app/src/main/java/android/project/longnt/giftchoiceguidance/MainActivity.java
@@ -0,0 +1,11 @@
+package android.project.longnt.giftchoiceguidance;
+
+import android.os.Bundle;
+
+public class MainActivity extends AbsActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
