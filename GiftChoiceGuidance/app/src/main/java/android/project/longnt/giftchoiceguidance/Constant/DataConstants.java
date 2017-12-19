996ec57b68ba5b76b6a3ce10ecb13f9e93e2ee77
LongNT <longnt@ubuntu.ubuntu-domain>
2017-12-20 00:28:56 +0100

    1-Add DB design. 2-Create principle packages

---
+++ GiftChoiceGuidance/app/src/main/java/android/project/longnt/giftchoiceguidance/Constant/DataConstants.java
@@ -0,0 +1,11 @@
+package android.project.longnt.giftchoiceguidance.Constant;
+
+/**
+ * Created by longnt on 12/19/17.
+ */
+
+public class DataConstants {
+    // ======================== DATABASE CONSTANTS ====================== //
+    public static final String DB_DATABASE_NAME = "GiftChoiceDB";
+
+}
