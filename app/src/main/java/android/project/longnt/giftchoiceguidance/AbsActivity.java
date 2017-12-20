0a013447835ed05373a319dbbabe19b127b8c561
thanh-long.nguyen <thanh-long.nguyen@horanet.com>
2017-12-20 17:48:17 +0100

    no msg

---
+++ app/src/main/java/android/project/longnt/giftchoiceguidance/AbsActivity.java
@@ -0,0 +1,10 @@
+package android.project.longnt.giftchoiceguidance;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LongNT on 19/12/17.
+ */
+
+public abstract class AbsActivity extends AppCompatActivity {
+}
